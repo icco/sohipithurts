d72051ec7b6b9a9ccac34a0f7aed1c695217b00d
Nat Welch <nat@natwelch.com>
2014-04-21 08:49:20 -0700

    dumb camera is dumb

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,13 @@
 Crafty.init(800, 600, 'game');
 
 Crafty.e('Floor, 2D, Canvas, Color')
-  .attr({x: 0, y: 250, w: 250, h: 10})
+  .attr({x: 0, y: 250, w: 900, h: 10})
   .color('green');
 
-Crafty.e('2D, Canvas, Color, Fourway, Gravity')
+guy = Crafty.e('2D, Canvas, Color, Fourway, Gravity, Guy')
   .attr({x: 0, y: 0, w: 50, h: 50})
   .color('#F00')
   .fourway(4)
   .gravity('Floor');
+
+Crafty.viewport.follow(guy, 0, 0);
