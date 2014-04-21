5d66dd6889cf6697bca845031a9f7cca15487565
Nat Welch <nat@natwelch.com>
2014-04-21 04:34:27 -0700

    box

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,11 @@
 Crafty.init(500,350, document.getElementById('game'));
+
+Crafty.e('Floor, 2D, Canvas, Color')
+  .attr({x: 0, y: 250, w: 250, h: 10})
+    .color('green');
+
+    Crafty.e('2D, Canvas, Color, Fourway, Gravity')
+      .attr({x: 0, y: 0, w: 50, h: 50})
+    .color('#F00')
+      .fourway(4)
+    .gravity('Floor');
