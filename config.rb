57d6d6320b81d6607abc91c79790bc2e843dfa96
Nat Welch <nat@natwelch.com>
2014-04-21 03:52:20 -0700

    tweak config

--- config.rb
+++ config.rb
@@ -23,42 +23,31 @@
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
 # Reload the browser automatically whenever files change
 activate :livereload
 activate :bower
 activate :autoprefixer
 
-set :slim, pretty: true
-
 # default directories
-#set :js_dir, 'javascripts'
-#set :css_dir, 'stylesheets'
-#set :images_dir, 'images'
+set :js_dir, 'js'
+set :css_dir, 'css'
+set :images_dir, 'img'
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def some_helper
+    "Helping"
+  end
+end
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :asset_hash
 end
